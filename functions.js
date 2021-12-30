f3ad4d8d1fcc407332182de5172eaf488b6b6433
Raiza De Guzman <santos_dg7@hotmail.com>
2021-12-29 20:09:53 -0800

    function exercises in Prep Module 4 with same name

---
+++ functions.js
@@ -0,0 +1,15 @@
+// function reverseString(reverseMe) {
+//   var reversed = "";
+//   for (var i = reverseMe.length - 1; i >= 0; i--) {
+//     reversed += reverseMe[i];
+//   }
+//   return reversed;
+// }
+
+// console.log(reverseString("Julia"));
+
+function sayHello() {
+  var message = "Hello!";
+  return message;
+}
+console.log(sayHello());
